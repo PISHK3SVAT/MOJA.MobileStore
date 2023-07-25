bca278953011143e988074eed18e1935e4aeb382
PISHK3SVAT <mjek106206@gmail.com>
2023-07-25 19:26:24 +0330

    Admin Endpoint added Themes for site and admin added

---
+++ MOJA.Mobile.Admin.Endpoint.mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MOJA.Mobile.Admin.Endpoint.mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
