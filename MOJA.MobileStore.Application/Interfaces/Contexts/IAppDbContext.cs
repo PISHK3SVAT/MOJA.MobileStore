3f6e7af9cbf2aa6cc8bb529e5288cac81fd7e2f6
PISHK3SVAT <mjek106206@gmail.com>
2023-07-28 13:04:06 +0330

    Init IAppDbContext

---
+++ MOJA.MobileStore.Application/Interfaces/Contexts/IAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using MOJA.MobileStore.Domain.Entities.Products;
+
+namespace MOJA.MobileStore.Application.Interfaces.Contexts
+{
+    public interface IAppDbContext
+    {
+        DbSet<Product> Products { get; set; }
+        int SaveChanges();
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ MOJA.MobileStore.Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using MOJA.MobileStore.Application.Interfaces.Contexts;
+using MOJA.MobileStore.Domain.Entities.Products;
+
+namespace MOJA.MobileStore.Persistence.Contexts
+{
+    public class AppDbContext:DbContext,IAppDbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
